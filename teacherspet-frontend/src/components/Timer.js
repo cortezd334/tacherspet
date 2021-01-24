bf93daf62455428e294fe6988f1eb6fb67ddec46
cortezd334 <cortezd334@gmail.com>
2021-01-24 11:41:27 -0800

    progress outside of box

--- teacherspet-frontend/src/components/Timer.js
+++ teacherspet-frontend/src/components/Timer.js
@@ -1,45 +1,12 @@
 import React, { useState } from 'react';
 
-const Timer = ({ start }) => {
-
-    const [remainder, setRemainder] = useState(0)
-    const [after, setAfter] = useState(0)
-    const [play, setPlay] = useState(false)
+const Timer = ({ start, remainder, after }) => {
        
     let prog = (remainder/start) * 300
     let newProg = (after/start) * 300
-    let time
-
-    const handleClick = () => {
-
-        setRemainder(start)
-        setPlay(true)
-        
-        if(after > 0) {
-            time = setInterval(() => {
-                setAfter(after => after - 1);
-            }, 1000);
-            return () => clearInterval(time);
-        } else {
-            time = setInterval(() => {
-                setRemainder(remainder => remainder - 1);
-            }, 1000);
-            return () => clearInterval(time);
-        }
-    }
-
-    const clickHandler = () => {
-        setAfter(remainder)
-        setPlay(false)
-    }
 
     return (
         <>
-            {play ?
-                <button className='tBtn' onClick={clickHandler}>Pause</button>
-                :
-                <button className='tBtn' onClick={handleClick}>Start</button>
-            }
             { after > 0 ?
             (<div className='progress'>
                 <div style={{width: `${newProg}px`}} className='bar'></div>
