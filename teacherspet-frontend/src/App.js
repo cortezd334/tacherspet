cec64dd8a8345e4aa6a4bc84453906cacc9ccdd4
ellenupark <ellenupark@gmail.com>
2021-01-23 19:58:11 -0500

    Added side nav

--- teacherspet-frontend/src/App.js
+++ teacherspet-frontend/src/App.js
@@ -9,17 +9,22 @@ import TeacherContainer from './containers/TeacherContainer'
 import DisplayContainer from './containers/DisplayContainer'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
+import SideNavBar from './components/SideNavBar'
 
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path='/' component={NavBar} />
+        <Switch>
+          <Route exact path='/' component={NavBar} />
+          <Route path='/' component={SideNavBar} />
+        </Switch>
         <Switch>
           <Route exact path = '/' component={Home} />
           <Route exact path = '/teacher' component={TeacherContainer} />
           <Route exact path = '/display' component={DisplayContainer} />
+          <Route exact path = '/lessons' component={DisplayContainer} />
         </Switch>
       </div>
     </Router>
